d68518c92cc70d9df85b6521241b94a83040a23f
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2018-12-30 17:08:59 -0800

    add sample data to test

---
+++ src/models/listOfApi.js
@@ -0,0 +1,18 @@
+// list of APIS
+let apis = [
+  {
+    name: "PetStore",
+    url: "https://rebilly.github.io/ReDoc/swagger.yaml"
+  },
+  {
+    name: "Instagram",
+    url: "https://api.apis.guru/v2/specs/instagram.com/1.0.0/swagger.yaml"
+  },
+  {
+    name: "Google Calendar",
+    url:
+      "https://api.apis.guru/v2/specs/googleapis.com/calendar/v3/swagger.yaml"
+  }
+];
+
+export default apis;
